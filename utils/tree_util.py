a6398b940f58f1d657bd4692bf04fe2381b64d6f
Michael Silva <michaeldf.ti@gmail.com>
2021-03-21 17:22:15 -0300

    add important logs in check_accuracy

--- utils/tree_util.py
+++ utils/tree_util.py
@@ -44,6 +44,7 @@ def check_accuracy(
     y_batches = np.array_split(y_shuffled, k)
 
     for i in range(k):
+        print(f'    k-fold iteration: {i + 1}/{k} ', end='', flush=True)
         x_test, y_test = x_batches[i], y_batches[i]
         x_train = np.vstack([batch for j, batch in enumerate(x_batches) if j != i])
         y_train = np.hstack([batch for j, batch in enumerate(y_batches) if j != i])
@@ -51,9 +52,11 @@ def check_accuracy(
         if header is not None:
             model.fit(x_train, y_train, h=header)
             accuracy = 1 - model.eval(x_test, y_test, header)
+            print(f'accuracy {100 * accuracy:.3f}%')
         else:
             model.fit(x_train, y_train)
             accuracy = 1 - model.eval(x_test, y_test)
+            print(f'accuracy {100 * accuracy:.3f}%')
 
         total_accuracy += accuracy
         root = model.root if hasattr(model, 'root') else None
